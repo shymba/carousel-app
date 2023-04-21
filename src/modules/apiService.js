5de879222875862fbf2bcf3a8716a2334e2923ec
Alexander <shymbabigmir@gmail.com>
2023-04-21 11:23:15 +0300

    Some fix

--- src/modules/apiService.js
+++ src/modules/apiService.js
@@ -3,7 +3,12 @@ export default class ApiService {
     _apiBase = 'https://picsum.photos/';
 
     async getListImg() {
-        const response_list = await fetch(`${this._apiBase}v2/list`).then((res) => res.json());
+        const response_list = await fetch(`${this._apiBase}v2/list`, {
+            params: {
+                _page: 1,
+                _limit: 30
+            }
+        }).then((res) => res.json());
         return response_list
     }
 }
